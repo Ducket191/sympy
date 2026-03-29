dab1fbfa14f17b879e7f8c0bc55289bee3a87180
Ducket191 <dangminhduc1912008@gmail.com>
2026-03-29 17:16:09 +0700

    add sub handling

--- sympy/vector/vector.py
+++ sympy/vector/vector.py
@@ -588,6 +588,15 @@ class Cross(Expr):
             return Vector.zero
         return Expr.__radd__(self, other)
 
+    def __sub__(self, other):
+        if other == self:
+            return Vector.zero
+        return Expr.__sub__(self, other)
+
+    def __rsub__(self, other):
+        if other == self:
+            return Vector.zero
+        return Expr.__rsub__(self, other)
 
     def doit(self, **hints):
         return cross(self._expr1, self._expr2)
